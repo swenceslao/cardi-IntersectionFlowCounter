4be1acd181c6abe127b626664e04413fa66be202
pamjane <44188724+pamjane@users.noreply.github.com>
2018-10-25 09:47:15 +0800

    Update cartracker.py

--- cartracker.py
+++ cartracker.py
@@ -6,6 +6,7 @@ import imutils
 from collections import deque
 import argparse
 import pandas as pd
+import random
 
 
 cap = cv2.VideoCapture('cardi.MP4')
@@ -38,138 +39,69 @@ print(frames_count, fps, width, height)
 df = pd.DataFrame(index=range(int(frames_count)))
 df.index.name = "Frames"
 
+
+
 framenumber = 0  # keeps track of current frame
 carids = []  # blank list to add car ids
 totalcars = 0  # keeps track of total cars
 
 
-
-
 #capturing data
 while(True):
 
+        
+
+
+
 # Capture two frames
     ret, frame1 = cap.read()  # first image
+    
     time.sleep(1/25)          # slight delay
     ret, frame2 = cap.read()  # second image 
     image = cv2.resize(frame1, (0, 0), None, 1,1) 
 
+
+
 #getting the difference as the basis for movement
-    diff = cv2.absdiff(frame1,frame2)   
+    diff = cv2.absdiff(frame1,frame2)
     mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    th = 10
-    imask =  mask > th
+    th = 17
+    imask =  diff > th
     canvas = np.zeros_like(frame2, np.uint8)
     canvas[imask] = frame1[imask]
     mask = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
-# convert the grayscale image to binary image
-    ret,thresh = cv2.threshold(mask,0,255,0)
-
-
-# creates contours/blobs
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #canvas = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     
-# use convex hull to create polygon around contours
-    hull = [cv2.convexHull(c) for c in contours]
+    fgmask = fgbg.apply(mask)  # uses the background subtraction
 
-# draw contours
-    cv2.drawContours(mask, hull, -1, (0, 255, 0), 3)
+        # applies different thresholds to fgmask to try and isolate cars
+        
+        # just have to keep playing around with settings until cars are easily identifiable
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25,25))  # kernel to apply to the morphology
     
-    cxx = np.zeros(len(contours))
-    cyy = np.zeros(len(contours))
-
-
-
-# line created to stop counting contours, needed as cars in distance become one big contour
-    lineypos = 400
-    cv2.line(image, (-100, lineypos), (width, -120), (255, 0, 0), 3)   # bluee
-    #cv2.line(canvas, (-50, 100), (width, 100), (255, 0, 0), 5)
-    #cv2.line(image, (-50, 700), (width, 700), (0, 0, 255), 3)   # red
-
-# line y position created to count contours
-    lineypos2 = -700
-    cv2.line(image, (-150, lineypos2), (width, 700), (0, 255, 0), 3)    # green
-    cv2.line(image, (-150, -100), (width, 1800), (255, 255,0), 3)
     
-    for j in range(len(contours)): 
-
-        if hierarchy[0, j, 3] == -1:
-            cnt=contours[j]
-            
-            area = cv2.contourArea(cnt)
-           
-            if area > 500:
-                x,y,w,h = cv2.boundingRect(cnt)
-                cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-#getting variables for the centroids
-                cx = x + w/2
-                cy = y + h/2
-                cen = (cx,cy)
-                cv2.circle(image, (cx,cy), 7, (255,0,0), -1)
-
-                cv2.putText(image, str(cx) + "," + str(cy), (cx + 10, cy + 10), cv2.FONT_HERSHEY_SIMPLEX,
-                                    .5, (0, 0, 255), 1)
-                
-
-                cxx[j] = cx
-                cyy[j] = cy
-                pts.appendleft(cen)
-    #storing centroid positions
-        # eliminates zero entries (centroids that were not added)
     
-    for i in np.arange(1, len(pts)):
-                # if either of the tracked points are None, ignore
-                # them
-                if pts[i - 1] is None or pts[i] is None:
-                    continue
-         
-                # check to see if enough points have been accumulated in
-                # the buffer
-                if counter >= 10 and i == 1 and pts[-10] is not None:
-                    # compute the difference between the x and y
-                    # coordinates and re-initialize the direction
-                    # text variables
-                    dX = pts[-10][0] - pts[i][0]
-                    dY = pts[-10][1] - pts[i][1]
-                    (dirX, dirY) = ("", "")
-         
-                    # ensure there is significant movement in the
-                    # x-direction
-                    if np.abs(dX) > 10:
-                        dirX = "East" if np.sign(dX) == 1 else "West"
-         
-                    # ensure there is significant movement in the
-                    # y-direction
-                    if np.abs(dY) > 10:
-                        dirY = "North" if np.sign(dY) == 1 else "South"
-         
-                    # handle when both directions are non-empty
-                    if dirX != "" and dirY != "":
-                        direction = "{}-{}".format(dirY, dirX)
-         
-                    # otherwise, only one direction is non-empty
-                    else:
-                        direction = dirX if dirX != "" else dirY
-                    # otherwise, compute the thickness of the line and
-             
+    
+    dilation = cv2.dilate(fgmask, kernel)  
+    opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
+  
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    
+   # c = cv2.morphologyEx(d, cv2.MORPH_CLOSE, kernel)
 
-                # draw the centroid tracker
-                cv2.circle(image, (pts[i - 1]), 2, (0,0,255), -1)
+    im =closing
 
-
-              
-
-            
+   
 
 
 
 
 
-
-
-    cv2.imshow('Intersection Flow Prediction',image)
+    framenumber = framenumber + 1
+    cv2.imshow('Intersection Flow Prediction',im)
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
 
 cap.release()
 cv2.destroyAllWindows()
+df.to_csv('grounddata.csv', sep=',')
